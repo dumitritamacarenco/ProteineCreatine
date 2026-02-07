8de1f6b92fe1df971376b3a1638a66ea8075addb
Dumi <dumi@scrappzero.com>
2026-02-07 14:06:47 +0100

    Initial Commit

---
+++ ProteineCreatineApp/ProteineCreatineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProteineCreatineAppApp.swift
+//  ProteineCreatineApp
+//
+//  Created by New  on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ProteineCreatineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
